1dab24b1c2eb19b4acb9125c121067358b8f61bb
Kewko Alexoff <kehtabp@gmail.com>
2023-11-04 22:04:49 +0000

    Fix

--- standby-monitor.sh
+++ standby-monitor.sh
@@ -16,31 +16,32 @@ check_ping() {
 }
 
 check_vpn() {
-	local last_shake=$(docker exec wireguard wg show all latest-handshakes | jq -r '.[].latest_handshake')
-	local diff=$(( $(date +%s) - last_shake ))
-	if (( diff < 600 )); then
-		printf 'Sleep inhibited by an active VPN session %d seconds ago\n' "$diff" | logger -t standby-monitor -s
-		rm "$IDLE_COUNT_FILE" 2> /dev/null || true
-		exit 0
-	fi
+	last_shakes=$(docker exec wireguard wg show all latest-handshakes | cut -d$'\t' -f 3)
+	IFS=' ' read -ra ADDR <<< "$last_shakes"
+	for last_shake in "${ADDR[@]}"; do
+		echo "Last shake : $last_shake"
+		diff=$(( $(date +%s) - last_shake ))
+		if (( diff < 600 )); then
+			printf 'Sleep inhibited by an active VPN session %d seconds ago\n' "$diff" | logger -t standby-monitor -s
+			rm "$IDLE_COUNT_FILE" 2> /dev/null || true
+			# exit 0
+		fi
+	done
 }
 
 check_user_session() {
 	if who | grep -q .; then
 		printf 'Sleep inhibited by an active user session\n' | logger -t standby-monitor -s
 		rm "$IDLE_COUNT_FILE" 2> /dev/null || true
-		exit 0
+		# exit 0
 	fi
 }
 
 check_inhibit_flag() {
-	local sleep_set=$(curl -sf "$INHIBIT_URL")
-	local inhibit=($(echo "$sleep_set" | jq -e -r '.inhibit, .count'))
-	printf '%s\n' "${inhibit[1]}"
-	if (( inhibit[0] == 1 )); then
+	if (( $INHIBIT == 1 )); then
 		printf 'Sleep inhibited by the flag\n' | logger -t standby-monitor -s
 		rm "$IDLE_COUNT_FILE" 2> /dev/null || true
-		exit 0
+		# exit 0
 	fi
 }
 
@@ -49,7 +50,7 @@ check_plex_activity() {
 	if (( res > 0 )); then
 		printf 'Sleep inhibited by Plex activity\n' | logger -t standby-monitor -s
 		rm "$IDLE_COUNT_FILE" 2> /dev/null || true
-		exit 0
+		# exit 0
 	fi
 }
 
@@ -58,7 +59,7 @@ check_qbittorrent_downloads() {
 	if [[ -n "$active_downloads" ]]; then
 		printf 'Sleep inhibited by active downloads in qBittorrent with download speed less than 500kb/s\n' | logger -t standby-monitor -s
 		rm "$IDLE_COUNT_FILE" 2> /dev/null || true
-		exit 0
+		# exit 0
 	fi
 }
 
@@ -67,26 +68,22 @@ check_idle() {
 	(( idle_count++ ))
 	printf 'Been idle %d times\n' "$idle_count" | logger -t standby-monitor -s
 	echo "$idle_count" > "$IDLE_COUNT_FILE"
-	local sleep_set=$(curl -sf "$INHIBIT_URL")
-	local inhibit=($(echo "$sleep_set" | jq -e -r '.inhibit, .count'))
-	if (( idle_count >= inhibit[1] )); then
-		printf 'Sleeping\n' | logger -t standby-monitor -s
-		rm "$IDLE_COUNT_FILE"
-		if [ -f /sys/class/rtc/rtc0/wakealarm ]; then
-			local wakealarm=$(cat /sys/class/rtc/rtc0/wakealarm)
-			if [ -n "$wakealarm" ]; then
-				printf 'Next wakeup is scheduled for %s\n' "$(date -d "@$wakealarm")" | logger -t standby-monitor -s
-			else
-				/usr/sbin/rtcwake -m no -u -t "$(date +\%s -d "$(date +\%D -d '3 hours ago') +1 day 2 hours 59 minutes")"
-			fi
+	printf 'Sleeping\n' | logger -t standby-monitor -s
+	rm "$IDLE_COUNT_FILE"
+	if [ -f /sys/class/rtc/rtc0/wakealarm ]; then
+		local wakealarm=$(cat /sys/class/rtc/rtc0/wakealarm)
+		if [ -n "$wakealarm" ]; then
+			printf 'Next wakeup is scheduled for %s\n' "$(date -d "@$wakealarm")" | logger -t standby-monitor -s
 		else
-			printf 'Cannot access the wakealarm file\n' | logger -t standby-monitor -s -p user.err
+			/usr/sbin/rtcwake -m no -u -t "$(date +\%s -d "$(date +\%D -d '3 hours ago') +1 day 2 hours 59 minutes")"
 		fi
-		/usr/sbin/rtcwake -m no -u -t "$(date +\%s -d "$(date) +1 hour")"
-		# pm-suspend
+	else
+		printf 'Cannot access the wakealarm file\n' | logger -t standby-monitor -s -p user.err
 	fi
+	/usr/sbin/rtcwake -m no -u -t "$(date +\%s -d "$(date) +1 hour")"
+	# pm-suspend
 }
-
+# sleep $DELAY
 check_ping
 check_vpn
 check_user_session
