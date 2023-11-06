3a6ac5b14e1bb51afa0cecd7087ec774cf0712b8
Kewko Alexoff <kehtabp@gmail.com>
2023-11-06 11:36:22 +0000

    KIllall on other run

--- standby-monitor.sh
+++ standby-monitor.sh
@@ -12,14 +12,15 @@ script_dir=$(dirname $script_path)
 LOCKFILE=/dev/shm/${script_filename}.lock
 
 if [ -e "${LOCKFILE}" ]; then
-	printf 'Another instance of the script is running' | logger -t $script_filename -s
+	printf 'Another instance of the script is running. KIlling it.' | logger -t $script_filename
+	killall -q -TERM "${script_filename}"
 	exit 1
 else
 	touch "${LOCKFILE}"
 fi
 
 dont_sleep() {
-	# echo "Re-running" | logger -t $script_filename -s
+	# echo "Re-running" | logger -t $script_filename
 	rm "${LOCKFILE}"
     # "$script_path" &
 	exit 0
@@ -31,10 +32,10 @@ check_ping() {
 	if ! ping -c2 "$PING_DOMAIN" &> /dev/null; then
 		local last_reboot=$(cat /tmp/lastReboot)
 		local reboot_delta=$(( $(date +%s) - last_reboot ))
-		printf 'Failed to ping %s. Last reboot was %d sec ago.' "$PING_DOMAIN" "$reboot_delta" | logger -t $script_filename -s
+		printf 'Failed to ping %s. Last reboot was %d sec ago.' "$PING_DOMAIN" "$reboot_delta" | logger -t $script_filename
 		if (( reboot_delta > 10800 )); then
 			date +%s > /tmp/lastReboot
-			printf 'Rebooting' | logger -t $script_filename -s
+			printf 'Rebooting' | logger -t $script_filename
 			reboot
 		fi
 	fi
@@ -46,7 +47,7 @@ check_vpn() {
 		# echo "Last shake : $last_shake"
 		diff=$(( $(date +%s) - last_shake ))
 		if (( diff < 600 )); then
-			printf 'Sleep inhibited by an active VPN session %d seconds ago' "$diff" | logger -t $script_filename -s
+			printf 'Sleep inhibited by an active VPN session %d seconds ago' "$diff" | logger -t $script_filename
 			dont_sleep
 		fi
 	done
@@ -54,14 +55,14 @@ check_vpn() {
 
 check_user_session() {
 	if [[ `who | wc -l` -gt 0 ]]; then
-		printf 'Sleep inhibited by an active user session' | logger -t $script_filename -s		
+		printf 'Sleep inhibited by an active user session' | logger -t $script_filename		
 		dont_sleep
 	fi
 }
 
 check_inhibit_flag() {
 	if (( "$INHIBIT" == 1 )); then
-		printf 'Sleep inhibited by the flag' | logger -t $script_filename -s	
+		printf 'Sleep inhibited by the flag' | logger -t $script_filename	
 		dont_sleep
 	fi
 }
@@ -69,7 +70,7 @@ check_inhibit_flag() {
 check_plex_activity() {
 	local res=`curl -s "$PLEX_URL" | grep "MediaContainer size=\"0\"" | wc -l`
 	if ! (( res )); then
-		printf 'Sleep inhibited by Plex activity' | logger -t $script_filename -s
+		printf 'Sleep inhibited by Plex activity' | logger -t $script_filename
 		dont_sleep
 	fi
 }
@@ -77,7 +78,7 @@ check_plex_activity() {
 check_downloads() {
 	total_download_speed=$(curl -sf "http://${QB_URL}:${QB_PORT}/api/v2/transfer/info" | jq -r '.dl_info_speed')
 	if (( total_download_speed > 500000 )); then
-		printf 'Sleep inhibited by active downloads with download speed more than 500kb/s' | logger -t $script_filename -s
+		printf 'Sleep inhibited by active downloads with download speed more than 500kb/s' | logger -t $script_filename
 		dont_sleep
 	fi
 }
@@ -87,12 +88,12 @@ sleep_and_wake() {
 	if [ -f /sys/class/rtc/rtc0/wakealarm ]; then
 		local wakealarm=$(cat /sys/class/rtc/rtc0/wakealarm)
 		if [ -n "$wakealarm" ]; then
-			printf 'Next wakeup is scheduled for %s' "$(date -d "@$wakealarm")" | logger -t $script_filename -s
+			printf 'Next wakeup is scheduled for %s' "$(date -d "@$wakealarm")" | logger -t $script_filename
 		else
 			/usr/sbin/rtcwake -m no -u -t "$(date +\%s -d "$(date +\%D -d '3 hours ago') +1 day 2 hours 59 minutes")"
 		fi
 	else
-		printf 'Cannot access the wakealarm file' | logger -t $script_filename -s -p user.err
+		printf 'Cannot access the wakealarm file' | logger -t $script_filename -p user.err
 	fi
 	/usr/sbin/rtcwake -m no -u -t "$(date +\%s -d "$(date) +1 hour")"
 	rm "${LOCKFILE}"
@@ -100,7 +101,7 @@ sleep_and_wake() {
 }
 
 # Sleep for the specified delay
-echo "Started... Sleeping for $DELAY seconds." | logger -t $script_filename -s
+echo "Started... Sleeping for $DELAY seconds." | logger -t $script_filename
 sleep $DELAY
 check_ping
 check_vpn
